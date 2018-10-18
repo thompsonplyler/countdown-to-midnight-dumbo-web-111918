e223c3a70c88368c0f03b96b7740141fd79ef4c8
Thompson Plyler <nealok.g@gmail.com>
2018-10-18 21:44:54 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
+def countdown(num)
+  count = num
 
-def countdown
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    sleep(1)
+    count -= 1
+  end
+
+return "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(num)
+  count = num
+
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    sleep(1)
+    count -= 1
+  end
+
+return "HAPPY NEW YEAR!"
+
 end
